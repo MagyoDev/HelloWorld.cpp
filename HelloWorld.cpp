8ebe599774e9ec9f0a94416802b68faa656f86c7
MagyoDev <alandelimasilva51@gmail.com>
2024-03-07 09:00:05 -0300

    HelloWorld.cpp

---
+++ HelloWorld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    std::cout << "Air" << std::endl;
+    std::cout << "Water\n";
+    std::cout << "Earth\n";
+    std::cout << "Fire\n";
+    return 0;
+}
